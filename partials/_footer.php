4b6d812ef6491f9b896ebc815bfe34d192009312
mahir mehta <80514750+mahirmehta@users.noreply.github.com>
2022-05-05 23:23:56 +0530

    Delete partials directory

--- partials/_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer container-fluid bg-dark text-light">
-    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="https://github.com/" target="_blank" rel="noopener noreferrer">Omkar Mahir Akhil</a></p>
-</div>
