c7bbf86ab72f7bf10de7dd458213e407cea9d80b
mahir mehta <80514750+mahirmehta@users.noreply.github.com>
2022-05-05 23:19:36 +0530

    Update about.php

--- about.php
+++ about.php
@@ -49,7 +49,7 @@
             <div class="carousel-background"><img src="assets/img/slide/slide-3.jpg" alt=""></div>
             <div class="carousel-container">
               <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown mb-0">A project by</h2><a href="https://github.com/darshankparmar" target="_blank">Omkar Mahir Akhil</a></p>
+                <h2 class="animate__animated animate__fadeInDown mb-0">A project by</h2><a href="https://github.com/" target="_blank">Omkar Mahir Akhil</a></p>
                 <a href="index.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Get Started</a>
               </div>
             </div>
